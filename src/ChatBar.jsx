b6a0b4dba3c80b3241d834f0d724a8f1137e111f
James Dowell <james.s.dowell@gmail.com>
2018-09-04 21:26:41 +0000

    formatting

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,18 +4,20 @@ class ChatBar extends Component {
   render() {
     return (
       <footer className="chatbar">
-        <input 
-        onChange={this.props.onUserChange} 
-        value={this.props.currentUser.name} 
+        <input
+        type='text'
         className="chatbar-username" 
         placeholder="Your Name (Optional)" 
+        onChange={this.props.onUserChange} 
+        value={this.props.currentUser.name} 
         />
         <input 
         type='text' 
         className="chatbar-message" 
         placeholder="Type a message and hit ENTER" 
         value={this.props.value} 
-        onKeyPress={this.props.onKeyPress} onChange={this.props.onChange}
+        onKeyPress={this.props.onKeyPress} 
+        onChange={this.props.onChange}
         />
       </footer>
     )
