3123ac69aaa397a3e66de395d11201ec751c013b
James Dowell <james.s.dowell@gmail.com>
2018-09-04 04:55:58 +0000

    removed unnecessary constructor function calls

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <footer className="chatbar">
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -2,9 +2,6 @@ import React, {Component} from 'react';
 import Message from './Message.jsx';
 
 class MessageList extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     const messages = this.props.messages;
     const listitems = messages.map((message) => 
