7515fb1674ff8ee243aeec39bce90a8f985856cc
James Dowell <james.s.dowell@gmail.com>
2018-09-05 01:26:19 +0000

    cleared up dead code

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,11 @@ class App extends Component {
 
     this.messageGenerator = this.messageGenerator.bind(this);
     this.notificationGenerator = this.notificationGenerator.bind(this);
-    // this.onKeyPressName = this.onKeyPressName.bind(this);
 
     this.state = {
       currentUser: {name: "Bob"},
       value: '',
       messages: [],
-      notifications: []
     }
   }
 
@@ -32,27 +30,13 @@ class App extends Component {
       username: user, 
       content: message
     };
+
     if (this.state.currentUser.name !== user) {
       this.notificationGenerator(this.state.currentUser.name, user)
       this.setState({currentUser: {name: user}})
     }
     this.socket.send(JSON.stringify(newMessage));   
   }
-
-
-  // onKeyPressName(e) {
-  //   console.log('event target value', e.target.value);
-  //   console.log('current name', this.state.currentUser.name);
-  //   if (e.charCode === 13) {
-  //     const oldUsername = this.state.currentUser.name;
-  //     if (e.target.value !== oldUsername) {
-  //       this.setState({
-  //         currentUser: {name: e.target.value}
-  //       })
-  //       this.notificationGenerator(oldUsername);
-  //     }
-  //   }
-  // }
 
   componentDidMount() {
     this.socket = new WebSocket('ws://localhost:3001/');
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,17 +1,21 @@
 import React, {Component} from 'react';
 import Message from './Message.jsx';
-import Notification from './Notification.jsx';
+
 
 class MessageList extends Component {
   render() {
     const messages = this.props.messages;
+
     const listitems = messages.map((message) =>  {
       if (message.type === 'incomingMessage') {
         return (
-        <Message key={message.id} message={message}/>
+          <Message 
+            key={message.id} 
+            message={message}
+          />
         )
       } else if (message.type === 'incomingNotification') {
-        return(
+        return (
           <div className="message system">
             {message.content}
           </div>
@@ -19,6 +23,7 @@ class MessageList extends Component {
       }
     }
     )
+
     return (
       <main className="messages">
         <div>{listitems}</div>
