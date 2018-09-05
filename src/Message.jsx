f533b706c7b60a5a32c628b4ce8b9e89cbae64c1
James Dowell <james.s.dowell@gmail.com>
2018-09-05 18:08:43 +0000

    diplays images when users post links in chat

--- src/Message.jsx
+++ src/Message.jsx
@@ -6,11 +6,15 @@ class Message extends Component {
     let colorStyle= {
       color: this.props.message.color
     }
+    let regex = new RegExp(/([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/)
+    let url = this.props.message.content.match(regex);
     return (
       <div>
         <div className="message">
-          <span style={colorStyle}className="message-username">{this.props.message.username}</span>
-          <span className="message-content">{this.props.message.content}</span>
+          <span style={colorStyle}className="message-username">{ this.props.message.username }</span>
+          <span className="message-content">{ this.props.message.content }
+          {url && <img className='message-img' src={url[0]}/>}
+          </span>
         </div>
       </div>
     )
